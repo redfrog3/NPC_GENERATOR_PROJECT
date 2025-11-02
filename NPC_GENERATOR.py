f36e7eb4f86fae40996acab68d0603aad299c8ec
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2025-11-02 23:21:21 +0000

    FINAL COMMIT

--- NPC_GENERATOR.py
+++ NPC_GENERATOR.py
@@ -1,25 +1,32 @@
 import random
 import time
 
-user_input = int(input("Enter the amount of NPC's you would like to generate: "))  #Takes the amount of NPC's to print
-Name = ["Alex", "Taylor", "Riley", "Jordan", "Jamie", "Charlie", "Oliver"]
+while True:    #<--- makes the loop run endlessly
+    user_input = int(input("Enter the amount of NPC's you would like to generate: "))  #Takes the amount of NPC's to print
+    if user_input > 20:
+        print("\n-:-ERROR-:- A maximum of 20 NPC's can be printed at once, please enter another value\n") #<---- error if 21 or above NPC's are requested
+    elif user_input < 1:
+        print("\n-:-ERROR-:- Only positive numbers can be printed, please enter another value\n: ")   #<---- error if 0 or any negitive numbers are requested
+    else:
+        break    #<---- stops the loop
+  
+Name = ["Alex", "Taylor", "Riley", "Jordan", "Jamie", "Charlie", "Oliver", "sam", "kai", "Rowan", "Morgan", "corrie", "Jasper", "Luca", "Quin", "Jade", "Rune", "Java"] #<--- gender neutral names
 gender = ["Male", "Female"]                  
-for i in range(user_input): #<--- loops the NPC how ever many times user unput is
-    random_name = (random.choice(Name))          # Takes a random string from the list from line 6 and prints as NPC's name
+for i in range(user_input): #<--- loops the NPC generator how ever many times user unput is
+    random_name = (random.choice(Name))          #Takes a random string from the list in line 13 and prints as NPC's name
     random_gender = (random.choice(gender))
-    Attack_power = (random.randint(1, 10))                  #each of these characteristics print strength 1 - 10
+    Attack_power = (random.randint(1, 10))                  #each of these characteristics print strength 1/10 - 10/10
     Health_efficency = (random.randint(1, 10))              #^
     Agility = (random.randint(1, 10))                       #^
     sheilding = (random.randint(1, 10))                     #^
     NPC_stats = (f"-------------\nCharacter stats:\nName: {random_name} \nGender: {random_gender} \nHealth efficency:  {Health_efficency}/10  \nAttack power:  {Attack_power}/10 \nsheilding:  {sheilding}/10  \nAgility:  {Agility}/10")  #This is the format of the final print
-    time.sleep(0.888)
+    time.sleep(0.000888) #<---- Float
     print(NPC_stats)
     if Attack_power + Health_efficency + Agility + sheilding >= 40:          #adds Attack_power + Health_efficency + Agility + sheilding and determans it's strength   
         print(f"{random_name}" " is a GOLDEN CHARACTER and has top tier skill!")    #10/10
     elif 28 <= Attack_power + Health_efficency + Agility + sheilding <= 36:   
-        print(f"{random_name}" " is strong and has above avarage skill!") #7/10 - 9/10
+        print(f"{random_name}" " is strong and has above average skill!") #7/10 - 9/10
     elif 16 <= Attack_power + Health_efficency + Agility + sheilding <= 24:
-        print(f"{random_name}" " is standard and has avarage skill!")         #4/10 - 6/10
+        print(f"{random_name}" " is standard and has average skill!")         #4/10 - 6/10
     else:
-        print(f"{random_name}" " is weak and has below avarage skill!")       #1/10 - 3/10
-
+        print(f"{random_name}" " is weak and has below average skill!")       #1/10 - 3/10
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Semester_1_final_python_project
-NPC Character generator
+NPC generator
+
+🌟 **OVERVIEW** 🌟
+
+
+The NPC generator can be used to print a variety of different characters each with their very own skills and powers. Some will be female and some will be male, some will be more powerful than others, and each NPC will have their very own name. My program will ask the user for a certain number of NPC's, my program will then print the amount of NPC's the user suggests. If you enter over 20 NPC's, you will find that it gives an error saying that you are limited to 20 or below characters.
+
+
+🌟 **HIGHLIGHTS** 🌟
+
+
+- Takes user input
+- Prints variety of characteristics
+- Prints power range in a statement
+- Prints names
+- Gives error if user input is over 20
+- Gives error if user input is 0 or under
+
+
+🌟 **AUTHOR** 🌟
+
+
+I am Grayson Woodell https://github.com/redfrog3, a first year student in the Computer Science Engineering Academy https://github.com/CSAEA at Voorhees high school. This program was written with the basics of python with minimum experience for ease of use and great performance. THis program will help the client with infinite solutions to diverse non-playable characters for an open world video game.
+
+
+🌟 **USAGE INSTRUCTIONS** 🌟
+
+
+When the program has been initiated, it will ask the user for the amount of NPC's they would like to generate. Once you proceed to enter the number, if above 20, you will be prompted with an error, if 0 or below, you will be prompted with an error. If entered in the correct range <u>(1-20)</u>, You will be prompted with the amount of NPCs requested. From here the program will stop.
